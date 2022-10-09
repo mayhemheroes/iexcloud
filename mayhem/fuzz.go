4eb673e15cea70b24506146be650854380243b11
bmwtsn098 <bmwtsn99@gmail.com>
2022-10-09 13:52:42 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzz.go
@@ -0,0 +1,14 @@
+package fuzz
+
+import "github.com/goinvest/iexcloud/v2"
+
+func mayhemit(bytes []byte) int {
+    content := string(bytes)
+    _ = iex.WithBaseURL(content)
+    return 0
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
